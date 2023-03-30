81d004f1d7b63185c81a5fd455bbe67914a9201a
smarinv16 <funemesanchez@gmail.com>
2023-03-30 06:35:05 -0500

    Se crea el primer ejemplo

---
+++ funcionesLambda.py
@@ -0,0 +1,7 @@
+# Cambio de sintaxis
+
+saludar = lambda nombre, ocupacion : print(f'Bondiaaa {nombre} eres un {ocupacion}') 
+
+resultado = saludar('Juanchoo', 'Juanero')
+
+print(resultado)
---
+++ funcionesTradicionales.py
@@ -0,0 +1,7 @@
+def saludar(nombre, ocupacion):
+    return(f'Bon díaaa {nombre} y tu ocupacion es: {ocupacion}')
+
+
+resultado = saludar('Pedro', 'Alfarero')
+
+print(resultado)
