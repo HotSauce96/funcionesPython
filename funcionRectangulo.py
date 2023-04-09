ff58226ceee22d8f583e76271572d83816771169
Juan Pablo Jaramillo Giraldo <82364328+HotSauce96@users.noreply.github.com>
2023-04-08 20:46:43 -0500

    Update funcionRectangulo.py

--- funcionRectangulo.py
+++ funcionRectangulo.py
@@ -2,19 +2,19 @@
 
 def mostrarRectangulo():
      
-    ancho = int(input("Ingrese por favor la base o el ancho: "))
+    base = int(input("Ingrese por favor la base o el ancho: "))
     altura = int(input("Ingrese por favor la altura: "))
      
-    print("Tenemos en cuenta que el área de un rectángulo es base (En este caso Ancho) * altura, y que su perímetro se calcula como: Perimetro = 2 * base + 2 * altura")
+    print("Tenemos en cuenta que el área de un rectángulo es base * altura, y que su perímetro se calcula como: Perimetro = 2 * base + 2 * altura")
     
 # Se realizan las fórmulas de área y perímetro: 
 
-    a = ancho * altura
-    p = 2 * ancho + 2 * altura
+    a = base * altura
+    p = 2 * base + 2 * altura
     
     print(f"El área del rectángulo es: {a} y su perímetro es: {p}")
      
-    for i in range(ancho):
+    for i in range(base):
         linea = ""
         for j in range(altura):
             linea += "*" # Agregar un asterisco a la línea
