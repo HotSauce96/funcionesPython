c8bfa8516fd458788b56a62e4c5780f0ba7e923e
Juan Pablo Jaramillo Giraldo <82364328+HotSauce96@users.noreply.github.com>
2023-04-09 18:55:18 -0500

    Update functionTriangulo.py

--- functionTriangulo.py
+++ functionTriangulo.py
@@ -2,26 +2,32 @@
 
 def mostrarTriangulo():
      
-    base = int(input("Ingrese por favor la base o el ancho: "))
-    altura = int(input("Ingrese por favor la altura: "))
+    while True:
     
-    print("Para hallar el perímetro necesitamos saber la medida de los lados del triangulo")
-    
-    ladoUno = int(input("Ingrese por favor el primer lado: "))
-    ladoDos = int(input("Ingrese por favor el segundo lado: "))
-    ladoTres = int(input("Ingrese por favor el tercer lado: "))
-     
-    print("Debemos recordar que el área de un triángulo es base  * altura / 2; y que su perímetro se calcula como: Perimetro = Lado + Lado + Lado ")
-    
-    print("Debido a que existen diferentes tipos de triángulos, (Escaleno, Isoceles, Equilátero) La fórmula del perímetro no puede ser P = L * 3 (No todos lados tienen el mismo valor)")
-    
-# Se realizan las fórmulas de área y perímetro: 
+        base = int(input("Ingrese por favor los cm de la base o el ancho: "))
+        altura = int(input("Ingrese por favor los cm de la altura: "))
+        
+        print("Para hallar el perímetro necesitamos saber la medida de los lados del triangulo")
+        
+        ladoUno = int(input("Ingrese por favor los cm del primer lado: "))
+        ladoDos = int(input("Ingrese por favor los cm del segundo lado: "))
+        ladoTres = int(input("Ingrese por favor los cm del tercer lado: "))
+        
+        print("Debemos recordar que el área de un triángulo es base  * altura / 2; y que su perímetro se calcula como: Perimetro = Lado + Lado + Lado ")
+ #"Debido a que existen diferentes tipos de triángulos, (Escaleno, Isoceles, Equilátero) La fórmula del perímetro no puede ser P = L * 3 (No todos lados tienen el mismo valor)
+        
+ # Se realizan las fórmulas de área y perímetro: 
 
-    a = (base * altura) / 2
-    p = ladoUno + ladoDos + ladoTres
-    
-    print(f"El área del rectángulo es: {a} y su perímetro es: {p}")
-     
+        a = (base * altura) / 2
+        p = ladoUno + ladoDos + ladoTres
+        
+        if ladoUno == ladoDos and ladoUno == ladoTres: 
+            print(f"El área del rectángulo es: {a} cm^2, su perímetro es: {p} cm y es un triángulo equilátero.")
+        
+        elif ladoUno == ladoDos or ladoDos == ladoTres:
+            print(f"El área del rectángulo es: {a} cm^2, su perímetro es: {p} cm y es un triángulo isóceles.")
+        else: 
+            print(f"El área del rectángulo es: {a} cm^2, su perímetro es: {p} cm y es un triángulo escaleno.")
 
 # Se llama a la función: 
 
