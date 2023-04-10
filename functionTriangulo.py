2ce6985f17293ea032794f3bf8eed99d8b00f4a2
Juan Pablo Jaramillo Giraldo <82364328+HotSauce96@users.noreply.github.com>
2023-04-09 20:12:00 -0500

    Update functionTriangulo.py

--- functionTriangulo.py
+++ functionTriangulo.py
@@ -14,9 +14,9 @@ def mostrarTriangulo():
         ladoTres = int(input("Ingrese por favor los cm del tercer lado: "))
         
         print("Debemos recordar que el área de un triángulo es base  * altura / 2; y que su perímetro se calcula como: Perimetro = Lado + Lado + Lado ")
- #"Debido a que existen diferentes tipos de triángulos, (Escaleno, Isoceles, Equilátero) La fórmula del perímetro no puede ser P = L * 3 (No todos lados tienen el mismo valor)
+    #"Debido a que existen diferentes tipos de triángulos, (Escaleno, Isoceles, Equilátero) La fórmula del perímetro no puede ser P = L * 3 (No todos lados tienen el mismo valor)
         
- # Se realizan las fórmulas de área y perímetro: 
+    # Se realizan las fórmulas de área y perímetro: 
 
         a = (base * altura) / 2
         p = ladoUno + ladoDos + ladoTres
@@ -29,6 +29,14 @@ def mostrarTriangulo():
         else: 
             print(f"El área del rectángulo es: {a} cm^2, su perímetro es: {p} cm y es un triángulo escaleno.")
 
+#Ciclo para mostrar el triángulo 
+
+        for i in range(base):
+            linea = ""
+            for j in range(i+1):
+                linea += "*" # Agregar un asterisco a la línea
+            print(linea) # Imprimir la línea en la consola
+            
 # Se llama a la función: 
 
 mostrarTriangulo()
