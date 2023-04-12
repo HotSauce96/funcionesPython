187461a7421279384bf33da9f12a92ecfddca3ca
Juan Pablo Jaramillo Giraldo <82364328+HotSauce96@users.noreply.github.com>
2023-04-11 22:18:35 -0500

    Update temperaturas.py

--- temperaturas.py
+++ temperaturas.py
@@ -1,23 +1,38 @@
 # Función para calcular la temperatura media de el valle de aburrá a partir de 20 datos diarios de temperatura por 8 días diferentes. 
 
 # Los datos al ser del mismo tipo (Int o Float, según se decida) van a ir en una lista.
-# Estadísticamente hablando, la media es lo mismo que el promedio, por lo que se tomarían las 20 diferentes temperaturas, se sumarían y se dividirían por 20 para tener un promedio de la temperatura del día.
+# La media es lo mismo que el promedio, por lo que se tomarían las 20 diferentes temperaturas, se sumarían y se dividirían por 20 para tener un promedio de la temperatura del día.
 # Luego se tomarían los 8 promedios de los días calculados y se podría promediar igual la temperatura de la semana por ejemplo. 
 
 # La lista valores_diarios tendrá las 20 temperaturas y con la función sum() de python se suman los elementos de la lista y se dividen por 20, luego se almacena el resultado en la variable promedio_dia
 
-def calcular_temperatura(valores_diarios):
+def calcular_temp():
 	
-	suma = sum(valores_diarios)
-	promedio_dia = suma / 20
-	print(f"El promedio del día fue de: {promedio_dia}")
-	
-	promedio_semana = promedio_dia * 8 / 8 # La multplicación serían los otros 8 promedios de los días, en este ejemplo solo para mostrar la fórmula ya que la respuesta no tiene sentido.  
-	
-	print(f"El promedio de la semana fue de: {promedio_semana}")
+#Lista vacía que toma los valores del día (20) a través de un ciclo for que crea un diccionario que toma el input del usuario y lo guarda en cada posición.
 
-# Para calcular el promedio de la semana necesitaríamos entonces 8 promedios del día (Es decir la suma de 8 listas que tienen 20 valores cada una)
-# La fórmula del promedio de la semana sería igual promedio_semana = promedio_dia + promedio_dia_dos ... / 8
-# Como se necesitarían 20 listas en el parámetro de la función no quedaría limpio ni legible; y cuando se pide una lista en un input se deben separar los valores con comas para poder ser procesados por un ciclo for con el método split().
+#Luego con la función sumar de python suma la lista valores_dia y lo divide en el numero_diario_temperaturas
 
-calcular_temperatura([36, 34, 38, 36, 34, 30, 25, 34, 28, 29, 32, 36, 35, 30, 27, 26, 34, 36, 29, 34])
+    valores_dia = []
+    numero_diario_temperaturas = 20
+	
+    for i in range(0, numero_diario_temperaturas):
+        temperatura = {}
+        temperatura["primer_dia"] = int(input("Ingrese la primer temperatura: "))
+        valores_dia.append(temperatura["primer_dia"])
+        
+    promedio_dia = sum(valores_dia) / numero_diario_temperaturas
+    
+    print(f"el promedio del día fue: {promedio_dia}")
+
+
+    # dias_semana = 4
+    
+    # promedio_semana = []
+    
+    # for i in range(0, dias_semana):
+    #     valores_semana = []
+    #     valores_semana.append(valores_dia)
+        
+    #     promedio_semana = sum(valores_semana) / dias_semana
+
+calcular_temp()
